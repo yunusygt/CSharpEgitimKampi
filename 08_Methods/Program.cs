d71e82a9eaaf99236675d7dadd185eb649203790
yunusygt <yunusyigit950@gmail.com>
2025-04-26 21:32:44 +0300

    update

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Void Metotlar
+            // () - Bir yapının metot olup olmadığını açılır kapanır parantezin olduğundan anlarız.
+            // void methods
+            //Geriye değer döndürmeyen metotlar - void metotlar diye adlandırılır
+            // customer --> listeler,ekle,sil,güncelle
+            // Void
+
+            //void customerList()
+            //{
+            //    Console.WriteLine("Yunus Yiğit");
+            //    Console.WriteLine("Kenan Yiğit");
+            //    Console.WriteLine("Nuray Yiğit");
+            //}
+            //customerList();
+
+            // Alt alta bir kaç defa çıktı almak istersek yazdırılmasını istediğimiz değerin alt altra bir kaç defa daha çıktısının almasına neden olur. bunun faydası tekrardan kurtarmaya yarar.
+            //customerList();
+            //customerList();
+
+
+            //void sum()
+            //{
+            //    int x = 1;
+            //    int y = 2;
+            //    int z = x + y;
+            //    Console.WriteLine(z);
+            //}
+            //sum();
+            #endregion
+
+            #region Geriye değer döndürmeyen string parametreli metotlar
+
+            //void WriteMthod(string customerName)
+            //{
+            //    Console.WriteLine(customerName); 
+            //}
+            //WriteMthod("Yunus Yiğit");
+
+            //void customerCard(string name,string surName)
+            //{
+            //    Console.WriteLine("Müşteri: " + name + " " + surName);
+            //}
+            //customerCard("Yunus","Yiğit");
+            //customerCard("Kenan","Çınar");
+
+            // istediğim kadar müşteri çağırabişlirim.
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
