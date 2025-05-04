f0bad11a6b97f7f44b10c75e86d913ae67a72735
yunusygt <yunusyigit950@gmail.com>
2025-05-04 22:42:37 +0300

    create işlemleri

--- 09_DatabaseProject/Program.cs
+++ 09_DatabaseProject/Program.cs
@@ -15,37 +15,43 @@ namespace _09_DatabaseProject
             // Ado.net --> C# programlama dilinde SQL yapılarını kullanabilmemize olanak sağlayan bir çerçevedir.
 
 
-            Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
-            Console.WriteLine();
-            Console.WriteLine();
+            //Console.WriteLine("***** C# Veri Tabanlı Ürün-Kategori Bilgi Sistemi *****");
+            //Console.WriteLine();
+            //Console.WriteLine();
 
-            string tableNumber;
+            //string tableNumber;
 
-            Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("1-Kategoriler");
-            Console.WriteLine("2-Ürünler");
-            Console.WriteLine("3-Siparişler");
-            Console.WriteLine("4-Çıkış Yap");
-            Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
-            tableNumber = Console.ReadLine();
-            Console.WriteLine("---------------------------------------------------------------");
+            //Console.WriteLine("---------------------------------------------------------------");
+            //Console.WriteLine("1-Kategoriler");
+            //Console.WriteLine("2-Ürünler");
+            //Console.WriteLine("3-Siparişler");
+            //Console.WriteLine("4-Çıkış Yap");
+            //Console.Write("Lütfen getirmek istediğiniz tablo numarasını giriniz: ");
+            //tableNumber = Console.ReadLine();
+            //Console.WriteLine("---------------------------------------------------------------");
 
-            SqlConnection connection = new SqlConnection("Data Source=YANNIS;initial Catalog=EgitimKampiDb;integrated security=true");
-            connection.Open();
-            SqlCommand command = new SqlCommand("select * from TblCategory", connection);
-            SqlDataAdapter adapter = new SqlDataAdapter(command);
-            DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            connection.Close();
+            //SqlConnection connection = new SqlConnection("Data Source=YANNIS;initial Catalog=EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("select * from TblCategory", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //connection.Close();
 
-            foreach (DataRow row in dataTable.Rows)
-            {
-                foreach (var item in row.ItemArray)
-                {
-                    Console.Write(item.ToString());
-                }
-                Console.WriteLine();
-            }
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    foreach (var item in row.ItemArray)
+            //    {
+            //        Console.Write(item.ToString());
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+            //********************************************************************************************************************
+
+
 
             //SqlConnection connection = new SqlConnection("Data Source=YANNIS;initial Catalog=EgitimKampiDb;integrated security=true");
             //connection.Open();
@@ -64,7 +70,7 @@ namespace _09_DatabaseProject
             //    Console.WriteLine();
             //}
 
-            
+
 
 
 
--- 10_DatabaseCrud/Program.cs
+++ 10_DatabaseCrud/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,28 +18,45 @@ namespace _10_DatabaseCrud
             Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
             Console.WriteLine();
 
-            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
+
+            #region Kategori Ekleme İşlemi
+
+            //Console.Write("Eklemek istediğiniz kategori adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source=YANNIS;initial catalog=EgitimKampiDb;integrated security=true;");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("insert into TblCategory(CategoryName) values(@p1)",connection);
+            //command.Parameters.AddWithValue("@p1",categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+            //Console.Write("Kategori Başarıyla Eklendi!");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+
+            string productName;
+            decimal productPrice;
+
+            Console.Write("Ürün adı: ");
+            productName = Console.ReadLine();
+            Console.Write("Ürün fiyatı: ");
+            productPrice = decimal.Parse(Console.ReadLine());
+
+            SqlConnection connection = new SqlConnection("Data Source=YANNIS;initial catalog=EgitimKampiDb;integrated security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into TblProduct (ProductName,ProductPrice,ProductStatus) values (@productName,@productPrice,@productStatus)",connection);
+            command.Parameters.AddWithValue("@productName",productName);
+            command.Parameters.AddWithValue("@productPrice",productPrice);
+            command.Parameters.AddWithValue("@productStatus",true);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            Console.Write("Ürün Başarılı Bir Şekilde Eklendi!");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            #endregion
 
 
 
