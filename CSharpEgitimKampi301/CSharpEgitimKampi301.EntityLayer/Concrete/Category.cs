1d9efb2b0d16602abce8385c0bf96aa744eb10bc
yunusygt <yunusyigit950@gmail.com>
2025-05-18 23:59:06 +0300

    OOP Modülü - N Katmanlı Mimari

---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        /* Field - Variable - Property  */
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+
+
+
+    }
+}
---
+++ CSharpEgitimKampi301/CSharpEgitimKampi301.EntityLayer/Concrete/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+    }
+}
